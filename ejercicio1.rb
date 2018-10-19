38653f4300aed63d93228d46fb8da86e37512b8d
danielmoralesp <daniel_moralesp@hotmail.com>
2018-10-19 17:10:55 -0500

    exersice1

---
+++ ejercicio1.rb
@@ -0,0 +1,23 @@
+name_var_name_large = 15
+nameVar             = "my name"
+
+
+puts "#{name_var_name_large} " + nameVar
+
+puts "hola"
+puts "segundo hola"
+
+# este es un comentario
+
+puts " con comillas \"dobles\" "
+puts ' con comillas "simples" '
+
+
+variable1 = 16.to_s
+variable2 = "dos mil cuatrocientos"
+operacion = variable1 == variable2
+
+puts "hola #{variable2} " + "mundo #{variable1} -
+     resultado = #{operacion}"
+
+     
