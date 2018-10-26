9106625f2e17eff94b3dfa93052c235cc401d3d3
danielmoralesp <daniel_moralesp@hotmail.com>
2018-10-26 18:22:52 -0500

    string Class

---
+++ string.rb
@@ -0,0 +1,40 @@
+class String
+  def initialize(string)
+    @string = string
+  end
+end
+
+puts String.new("Hola")
+
+
+class Array
+
+  def push(string)
+    # agrega un nuevo elemento a la clase Array por medio del metodo push
+  end
+
+  def concat
+    # logica
+  end
+
+  def count
+
+  end
+
+  def delete
+    # logica para eliminar numeros repetidos en un array
+  end
+end
+
+a = Array.new
+a.push("Hola")
+
+class 2
+  def even?
+    # logica
+  end
+
+  def class
+    # logica para saber el nombre de la clase con que se construyo
+  end
+end
