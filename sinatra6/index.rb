f7e9fccb2a05702df7faec616abe5b65bfbb1caf
danielmoralesp <daniel_moralesp@hotmail.com>
2018-11-02 19:23:04 -0500

    Notes Initial Setup

--- sinatra6/index.rb
+++ sinatra6/index.rb
@@ -3,19 +3,36 @@ require "data_mapper"
 
 # Establecemos la conexion
 DataMapper::setup(:default, "sqlite://#{Dir.pwd}/index.sqlite")
+#DataMapper::setup(:default, "sqlite://#{Dir.pwd}/index.db")
+# DataMapper.setup(
+#   :default,
+#   ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db"
+# )
 
 # Definimos el modelo
-class Note
+class Notes
   include DataMapper::Resource
 
   property :id,         Serial    # An auto-increment integer key
   property :content,    Text, :required => true
   property :complete,   Boolean, :required => true, :default => 0
-  property :created_at, Datetime
-  property :updated_at, Datetime
+  property :created_at, DateTime
+  property :updated_at, DateTime
 end
 
-DataMapper.auto_updgrade!
+DataMapper.auto_upgrade!
 
-# rutas
-# CRUD
+class MiApp < Sinatra::Base
+  # Read (CRUD)
+  # Traer todas las notas y las vamos a mostrar al cliente
+  get '/' do
+    @notes = Notes.all :order => :id.desc
+    @title = "Todas las Notes"
+    erb :index
+  end
+
+
+  post '/' do
+
+  end
+end
